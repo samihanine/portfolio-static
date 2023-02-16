e1e3aafcbad5030961a7f36ee562fdf056a7ad38
Sam <64200816+samihanine@users.noreply.github.com>
2023-02-16 17:18:04 -0500

    build

--- _next/static/EazvQg4QNkJj0ypizMHDG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/68-91012782734f7e75.js","static/chunks/pages/index-0d786bdffc6ac469.js"],"/_error":["static/chunks/pages/_error-2c2a9bcd4124449a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
